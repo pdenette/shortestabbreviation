a33036afccef0ef0b4f07fbf0ae99af7479ccf6d
pdenette <pdenette@tampabay.rr.com>
2019-10-15 16:29:05 -0400

    InitialCommeit
    FilePull
    Array Creation

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace shortestabbreviation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string line in File.ReadLines(@".\list.txt"))
+            {
+                // Console.WriteLine(line);
+                string[] words = line.Split(' ');
+                foreach (String w in words)
+                {
+                    Console.WriteLine(w.Substring(0, 3));
+                }
+                if (words.Distinct().Count() != words.Count())
+                {
+                    Console.WriteLine("Duplicate Found");
+                }
+
+                break;
+            }
+        }
+    }
+}
